9ddbe6428df507832e4eb89a983efc55497d674b
Add Chan <addison95132@gmail.com>
2016-10-21 17:18:01 -0700

    added message about google and deleted some comments

--- processing.pl
+++ processing.pl
@@ -2,31 +2,60 @@ use 5.24.0;
 use warnings;
 use strict;
 
-my $month = "october";
-# my $month = `date +%B`;
+# change your types of expenses here:
+my %categories = (
+);
+
+# get date. If not on Linux, this won't work
+my $month = `date +%B`;
+chomp($month);
 my $totalMoney = 0;
-my $personal = 0;
-my $necessities = 0;
-my $gifts = 0;
 
 while(<>){
-    print if !/Total/;
-    if (/Total/) {next;}
+    print if !/Total/;  # echo back out each row
+    if (/Total/) {next;}  # ignore total row since recalculated
+
+    # get all types -> $1
+    if (my ($testing) = $_=~/.*\|.*\|\s(.*)\s\|/){
+        # check to make sure the type isn't a title
+        unless ($1 =~ /Type/ or $1 =~ /---/){
+            my $type = $1;
+            unless (defined $categories{$type}) {
+                $categories{${type}} = 0;
+                # print $type;
+            }
+        }
+    }
+
+    # add money
     if(/\$([0-9]+)/){
         $totalMoney += $1;
         my $currentMoney = $1;
-        if(/\|\spersonal\s/i){$personal += $currentMoney;}
-        # if(/necessities/i){$necessities += $currentMoney;}
-        if(/\|\snecessities\s/i){$necessities += $currentMoney;}
-        if(/\|\sgifts\s/i){$gifts += $currentMoney;}
+        foreach my $categoryName (keys %categories){
+            if(/\|\s$categoryName/i){
+                $categories{$categoryName} += $currentMoney;
+            }
+        }
     }
 }
 
-my $personalPercentage = $personal / $totalMoney;
-my $necessitiesPercentage = $necessities / $totalMoney;
-my $giftsPercentage = $gifts / $totalMoney;
+my $percentages = "";
+my $allCategories = "";
+my $index = 0; # bad hack but it works -- no comma and pipe on last in list
 
-my @getPicture = qq#curl -o ${month}.png "http://chart.apis.google.com/chart?cht=p&chd=t:${personalPercentage},${necessitiesPercentage},${giftsPercentage}&chs=600x300&chl=Personal:${personal}|Necessities:${necessities}|Gifts:${gifts}" > /dev/null 2>&1#;
+foreach my $categoryName (keys %categories){
+    $index++;
+    if ($index != keys %categories){
+        $percentages .= (($categories{$categoryName} / $totalMoney) . ",");
+        $allCategories .= $categoryName . ":" . $categories{$categoryName} . "|";
+    } else {
+        $percentages .= $categories{$categoryName} / $totalMoney;
+        $allCategories .= $categoryName . ":" . $categories{$categoryName};
+    }
+}
+
+# uses Google API. If you don't want google, comment out the next two lines
+my @getPicture = qq#curl -o ${month}.png "http://chart.apis.google.com/chart?cht=p&chd=t:$percentages&chs=600x300&chl=$allCategories" > /dev/null 2>&1#;
 system(@getPicture);
 
 print "Total | \$$totalMoney | - | -";
